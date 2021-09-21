f34296f3ae1a3ac273b00fa85a6faf3f5217acc1
alec_suzaine_pj <1702386@ub.edu.ph>
2021-09-21 05:49:20 +0000

    Compute average

---
+++ average.py
@@ -0,0 +1,7 @@
+"""Average of grades"""
+pr=int(input('Enter Prelims grade: '))
+mt=int(input('Enter Midterm grade: '))
+sf=int(input('Enter Semifinals grade: '))
+fn=int(input('Enter Finals grade: '))
+avg=(pr+mt+sf+fn)/4
+print('Average =',str(avg))
